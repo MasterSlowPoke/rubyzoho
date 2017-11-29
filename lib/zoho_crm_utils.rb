fe163baf680b20f3aba84338ea127a6e34b4c00e
Craig Sniffen <craigsniffen@craigsniffen.com>
2017-11-29 07:42:51 -0500

    implement ignore_fields_with_bad_names config

--- lib/zoho_crm_utils.rb
+++ lib/zoho_crm_utils.rb
@@ -10,7 +10,10 @@ module ZohoCrmUtils
       names.each do |name|
         n = name.class == Symbol ? name.to_s : name
         n.gsub!(/[()]*/, '')
-        raise(RuntimeError, "Bad field name: #{name}") unless method_name?(n)
+        unless method_name?(n)
+          next if RubyZoho.configuration.ignore_fields_with_bad_names
+          raise(RuntimeError, "Bad field name: #{name}")
+        end
         create_getter(klass, n)
         create_setter(klass, n)
       end
